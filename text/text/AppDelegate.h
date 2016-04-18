901abcf49625d08e461be7c5e2aa19c486600020
SuperHyl <18270824583@163.com>
2016-04-18 19:30:30 +0800

    第一次创建文件

---
+++ text/text/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  text
+//
+//  Created by SuperHyl on 16/4/18.
+//  Copyright © 2016年 hyl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
